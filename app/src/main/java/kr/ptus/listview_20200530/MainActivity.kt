ab9137cbb44ed69b899eed4bc2bc0dfb589acb57
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-30 15:46:38 +0900

    Adapter 객체 생성 => 리스트뷰 연결  - lateinit : 나주에 객체화 한다.  - 만들어둔 Adapter 생성자 실제 확인

--- app/src/main/java/kr/ptus/listview_20200530/MainActivity.kt
+++ app/src/main/java/kr/ptus/listview_20200530/MainActivity.kt
@@ -2,12 +2,15 @@ package kr.ptus.listview_20200530
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.ptus.listview_20200530.adapters.StudentsAdapter
 import kr.ptus.listview_20200530.datas.Student
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     val students = ArrayList<Student>()
+    lateinit var studentAdapter : StudentsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +24,10 @@ class MainActivity : AppCompatActivity() {
         students.add(Student("엄곤지", 1984, gender = false))
         students.add(Student("이승원", 1978, gender = true))
         students.add(Student("이현호", 1981, gender = true))
+
+        studentAdapter = StudentsAdapter(this, R.layout.students_list_item, students)
+
+        studentsListView.adapter = studentAdapter
+
     }
 }
