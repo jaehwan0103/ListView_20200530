d159d5496200c8bdab7ce7f969b0b71f87f3be84
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-30 15:09:40 +0900

    list view 뼈대 작업을 완료 했다.

---
+++ app/src/main/java/kr/ptus/listview_20200530/adapters/StudentsAdapter.kt
@@ -0,0 +1,33 @@
+package kr.ptus.listview_20200530.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.ptus.listview_20200530.R
+import kr.ptus.listview_20200530.datas.Student
+import java.util.ArrayList
+
+class StudentsAdapter(context: Context, resId: Int, list: List<Student>) :
+    ArrayAdapter<Student>(context, resId, list) {
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.students_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+
+}
